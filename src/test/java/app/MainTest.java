1d9f79a61c58ab68bef833d659fc96bfc6a4f622
ilovethebeatles <iakushkova.es@phystech.edu>
2024-12-14 01:47:57 +0300

    add files

---
+++ src/test/java/app/MainTest.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals("Hello World", Main.Hello());
+    }
+}
